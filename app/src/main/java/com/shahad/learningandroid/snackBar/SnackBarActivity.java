06af550e968d0c82e6cf1ec5676dcb2e56941fa7
shahad-mahmud <shahad9381@gmail.com>
2020-07-20 11:55:04 +0600

    snack bar with slide and coordinator layout

--- app/src/main/java/com/shahad/learningandroid/snackBar/SnackBarActivity.java
+++ app/src/main/java/com/shahad/learningandroid/snackBar/SnackBarActivity.java
@@ -6,6 +6,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -16,19 +17,18 @@ import com.shahad.learningandroid.R;
  */
 public class SnackBarActivity extends AppCompatActivity {
 
-    private MaterialButton snackBarButton;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snackbar);
 
-        snackBarButton = findViewById(R.id.snack_bar_button);
+        MaterialButton snackBarButton = findViewById(R.id.snack_bar_button);
+        CoordinatorLayout coordinatorLayout = findViewById(R.id.coordinatorLayout);
 
         snackBarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar snackbar = Snackbar.make(v, "This is a snack bar", Snackbar.LENGTH_INDEFINITE);
+                Snackbar snackbar = Snackbar.make(coordinatorLayout, "This is a snack bar", Snackbar.LENGTH_INDEFINITE);
                 snackbar.setAction("To ami ki korbo?", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -36,6 +36,8 @@ public class SnackBarActivity extends AppCompatActivity {
                                 Toast.LENGTH_LONG).show();
                     }
                 });
+
+                snackbar.setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE);
                 snackbar.show();
             }
         });
